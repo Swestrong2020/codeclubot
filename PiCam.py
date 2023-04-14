f85a40f8dfee7d08752632035afadbb0ce82af73
sweder <swestrong9@gmail.com>
2023-04-14 22:43:41 +0200

    changes

--- PiCam.py
+++ PiCam.py
@@ -2,39 +2,42 @@ from picamera2 import Picamera2, Preview
 import time
 import os
 
-parentDir = os.path.dirname(os.path.realpath(__file__))
+
+
 Camera = Picamera2()
 CameraConfig = Camera.create_preview_configuration()
 Camera.configure(CameraConfig)
 Camera.start_preview(Preview.QTGL)
 Camera.start()
-LocalTime = time.asctime
+
 StartTime = int(time.strftime("%H"))
 directory = StartTime 
 parentDir = os.path.dirname(os.path.realpath(__file__))
+LocalTime = (time.strftime("%a %b %d")).lower()
 
 time.wait(2)
 
 
-while LocalTime != "Mon Apr 17 9:25:00 2023":
+print("Give a data using the folowing format: Mon Apr 17")
+SetDate = input().lower
 
-    
+
+while LocalTime != SetDate:
 
     #sets dir name to the current hour
-
+    directory = time.strftime("%H")
     # checks if dir with time name exsit
     if os.path.exists(os.path.dirname(os.path.realpath(__file__))+"/"+int(time.strftime("%H"))):
-        directory = time.strftime("%H")
-        path = os.path.join(parent_dir, directory)
+        path = os.path.join(parentDir, directory)
         #makes dir
         os.makedirs(path)
+        Camera.capture_file(path+((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
     else:
-        Camera.capture_file(parentDir((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
+        path = os.path.join(parentDir, directory)
+        Camera.capture_file(path+((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
 
+    LocalTime = (time.strftime("%a %b %d")).lower()
 
-
-
-    
-    Camera.capture_file(((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
     time.sleep(60)
+
     
