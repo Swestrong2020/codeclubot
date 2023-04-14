0ff865372ad48a9802335af319dc0bacbbe6fab5
sweder <swestrong9@gmail.com>
2023-04-14 22:21:46 +0200

    now adds a dir named after the hour its made

--- PiCam.py
+++ PiCam.py
@@ -1,15 +1,40 @@
 from picamera2 import Picamera2, Preview
 import time
+import os
 
+parentDir = os.path.dirname(os.path.realpath(__file__))
 Camera = Picamera2()
 CameraConfig = Camera.create_preview_configuration()
 Camera.configure(CameraConfig)
 Camera.start_preview(Preview.QTGL)
 Camera.start()
 LocalTime = time.asctime
+StartTime = int(time.strftime("%H"))
+directory = StartTime 
+parentDir = os.path.dirname(os.path.realpath(__file__))
+
+time.wait(2)
+
 
 while LocalTime != "Mon Apr 17 9:25:00 2023":
-    time.sleep(2)
+
+    
+
+    #sets dir name to the current hour
+
+    # checks if dir with time name exsit
+    if os.path.exists(os.path.dirname(os.path.realpath(__file__))+"/"+int(time.strftime("%H"))):
+        directory = time.strftime("%H")
+        path = os.path.join(parent_dir, directory)
+        #makes dir
+        os.makedirs(path)
+    else:
+        Camera.capture_file(parentDir((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
+
+
+
+
+    
     Camera.capture_file(((time.asctime()).replace(" ", "_", -1).replace(":","-",-1))+".jpg")
     time.sleep(60)
     
